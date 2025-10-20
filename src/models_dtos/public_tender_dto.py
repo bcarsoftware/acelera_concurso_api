2a238198c667c21662f8d5f3c666e94d80e41cd5
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-10-20 10:23:38 -0300

    feat: public tender institue and work title to models

--- src/models_dtos/public_tender_dto.py
+++ src/models_dtos/public_tender_dto.py
@@ -8,6 +8,8 @@ class PublicTenderDTO(BaseModel):
     user_id: int
     tender_name: str
     tender_board: str
+    work_tile: str
+    institute: Optional[str] = None
     notice_link: Optional[str] = None
     tender_date: Optional[date] = None
     deleted: bool = False
--- src/models_responses/public_tender_response.py
+++ src/models_responses/public_tender_response.py
@@ -9,6 +9,8 @@ class PublicTenderResponse(BaseModel):
     user_id: int
     tender_name: str
     tender_board: str
+    work_tile: str
+    institute: Optional[str] = None
     notice_link: Optional[str] = None
     tender_date: Optional[date] = None
     deleted: bool = False

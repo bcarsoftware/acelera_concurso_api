87780126209f0e05c2ab85defdf5b155f2b5201d
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-10-21 18:45:51 -0300

    fix: service subject linked to repository

--- src/services/subject/service_subject.py
+++ src/services/subject/service_subject.py
@@ -2,24 +2,33 @@ from typing import List
 
 from src.models_dtos.subject_dto import SubjectDTO
 from src.models_responses.subject_response import SubjectResponse
+from src.repositories.subject.repository_subject import SubjectRepository
+from src.repositories.subject.repository_subject_interface import SubjectRepositoryInterface
 from src.services.subject.service_subject_interface import ServiceSubjectInterface
 from src.utils.managers.subject_manager import SubjectManager
 
 
 class ServiceSubject(ServiceSubjectInterface):
+    subject_repository: SubjectRepositoryInterface
+
+    def __init__(self) -> None:
+        self.subject_repository = SubjectRepository()
+
     async def create_subject(self, subject_dto: SubjectDTO) -> SubjectResponse:
         await SubjectManager.make_validation(subject_dto)
-        pass
+
+        return await self.subject_repository.create_subject(subject_dto)
 
     async def update_subject(self, subject_dto: SubjectDTO, subject_id: int) -> SubjectResponse:
         await SubjectManager.make_validation(subject_dto)
-        pass
+
+        return await self.subject_repository.update_subject(subject_dto, subject_id)
 
     async def get_subjects(self, tender_id: int) -> List[SubjectResponse]:
-        pass
+        return await self.subject_repository.get_subjects(tender_id)
 
     async def get_subject_by_name(self, tender_id: int, name: str) -> List[SubjectResponse]:
-        pass
+        return await self.subject_repository.get_subject_by_name(tender_id, name)
 
     async def delete_subject(self, subject_id: int) -> SubjectResponse:
-        pass
+        return await self.subject_repository.delete_subject(subject_id)
